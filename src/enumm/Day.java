7c8608c7fe10689eecd0be4cfd37e19ab3f49ccd
Mateus <mmachado.csp@gmail.com>
2016-06-26 01:18:45 -0300

    example ENUM

---
+++ src/enumm/Day.java
@@ -0,0 +1,5 @@
+package enumm;
+
+public enum Day {
+	SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
---
+++ src/enumm/MainEnum.java
@@ -0,0 +1,12 @@
+package enumm;
+
+public class MainEnum {
+	public static void main(String[] args) {
+
+		System.out.println(day(Day.SATURDAY));
+	}
+
+	public static Day day(Day monday) {
+		return monday;
+	}
+}
